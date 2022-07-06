adc1baee6c1651e1480e11f463af4dd62558b290
Chadley Maggott <chadleym@bbd.co.za>
2022-07-06 19:21:59 +0200

    basic login functionality up and running, security disabled

---
+++ backend/src/main/java/com/bbdgrad/blogsite/security/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.bbdgrad.blogsite.security;
+
+//@Configuration
+public class SecurityConfig {
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .mvcMatchers(HttpMethod.GET, "/**").permitAll()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .oauth2ResourceServer().jwt();
+//        return http.build();
+//    }
+}
