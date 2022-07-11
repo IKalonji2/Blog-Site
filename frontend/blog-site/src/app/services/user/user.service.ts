56227ed17b810373918fc8c30b251eb72a17dd7d
Mkhize <phindi.mkhize@vcontractor.co.za>
2022-07-11 19:33:03 +0200

    removed in memory package and added user end point

--- frontend/blog-site/src/app/services/user/user.service.ts
+++ frontend/blog-site/src/app/services/user/user.service.ts
@@ -10,27 +10,12 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class UserService {
   constructor(private http: HttpClient) {}
 
-  private userUrl = 'http://ec2-13-244-233-86.af-south-1.compute.amazonaws.com:5000/username'; 
+  private userUrl =
+    'http://ec2-13-244-233-86.af-south-1.compute.amazonaws.com:5000/username';
 
   getUser(): Observable<User> {
-    const user = of(USER);
+    const user = this.http.get<User>(this.userUrl);
+    console.log(user);
     return user;
   }
-
-//   private userUrl =
-//   'http://ec2-13-244-233-86.af-south-1.compute.amazonaws.com:5000/username';
-
-//   httpOptions = {
-//   headers: new HttpHeaders({
-//     'Content-Type': 'application/json',
-//     'Access-Control-Allow-Origin': '*',
-//     'Access-Control-Allow-Methods': 'GET',
-//   }),
-// };
-
-// getUser(): Observable<User> {
-//   const user = this.http.get<User>(this.userUrl);
-//   console.log(user)
-//   return user;
-// }
 }
