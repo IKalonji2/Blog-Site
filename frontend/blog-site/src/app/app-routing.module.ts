f4bd34bfb8110ab4a67e7562afa97da3f28a31a6
Mkhize <phindi.mkhize@vcontractor.co.za>
2022-07-13 13:57:48 +0200

    added landing page route

--- frontend/blog-site/src/app/app-routing.module.ts
+++ frontend/blog-site/src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { ViewArticleComponent } from './components/view-article/view-article.component'
+import { ViewArticleComponent } from './components/view-article/view-article.component';
+import { LandingComponentComponent } from './components/landing-component/landing-component.component';
 
 const routes: Routes = [
-  { path: 'article', component: ViewArticleComponent }
+  { path: 'article', component: ViewArticleComponent },
+
+  { path: '', component: LandingComponentComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
